05d0941f81706caea0bc10b29337767da86f208d
David Kwan <daves.kwan@gmail.com>
2019-08-04 23:09:23 -0400

    added webpack bundle analyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,14 @@
 const merge = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // analyze production build size
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // doesn't hang process
+      openAnalyzer: false,
+      reportFilename: 'bundle_sizes.html'
+    })
+  ]
 });

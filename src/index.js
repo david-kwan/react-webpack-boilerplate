c2ab434e8c13039c47307b345d2194a53d1470b3
David Kwan <daves.kwan@gmail.com>
2019-08-05 10:39:15 -0400

    added react-axe

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import DefaultErrorBoundary from './DefaultErrorBoundary';
 
 import './styles.css';
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe');
+
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundary>

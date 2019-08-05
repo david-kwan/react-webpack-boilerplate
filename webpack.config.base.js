28c67c648102ec5610e3b9118dc8e6bb246250a1
David Kwan <daves.kwan@gmail.com>
2019-08-04 23:45:28 -0400

    add jest

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,24 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader', // use babel loader to transform our code before creating output bundle
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: ['last 2 versions', 'not dead', 'not < 2%'],
-                useBuiltIns: 'entry'
-              }
-            ],
-            '@babel/preset-react'
-          ], // preset can be targetted to specific browsers to decrease bundle size, react
-          plugins: [
-            'react-hot-loader/babel', // preserve state on reload
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-syntax-dynamic-import' // lazy load
-          ]
-        }
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
